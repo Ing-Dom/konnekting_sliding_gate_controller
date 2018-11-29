938430bb8ccb3d14e706b5fb65e877b01cfb2d83
SirSydom <com@sirsydom.de>
2018-11-29 15:43:03 +0100

    -

--- software/proto_sgc_01/proto_sgc_01.ino
+++ software/proto_sgc_01/proto_sgc_01.ino
@@ -1,14 +1,42 @@
 #include <KonnektingDevice.h>
 #include "config.h"
+#include <Bounce2.h>
 
 
 // include device related configuration code, created by "KONNEKTING CodeGenerator"
 #include "kdevice_sgc.h"
 
 
+Bounce debouncer_in_closed = Bounce();
+Bounce debouncer_in_opened = Bounce(); 
+Bounce debouncer_in_barrier = Bounce(); 
 
-unsigned long mytime = 0;
-unsigned long mytime2 = 0;
+
+#define T1_CYCLETIME 25
+#define T2_CYCLETIME 50
+#define T3_CYCLETIME 100
+#define T4_CYCLETIME 500
+#define T5_CYCLETIME 1000
+unsigned long T1_last_run = 0;
+unsigned long T2_last_run = 0;
+unsigned long T3_last_run = 0;
+unsigned long T4_last_run = 0;
+unsigned long T5_last_run = 0;
+
+
+int out_openclose_cnt = 0;
+bool in_closed_lastvalues[8];
+
+enum gate_state 
+{ 
+   unknown,
+   opened,
+   opening,
+   closed,
+   closing,
+   intermediate_will_open,
+   intermediate_will_close
+} main_gate_state;
 
 
 
@@ -23,8 +51,8 @@ void knxEvents(byte index)
             {
                 Debug.println(F("Received 1 on cmd_close"));
 
-                //ToDo: check if 1
-                //If yes, set OUT_OPENCLOSE to 1 for 500ms
+                out_openclose_cnt = 20; // 20x 25ms = 500ms set OUT_OPENCLOSE to 1 for 500ms
+                
             }
             break;
 
@@ -76,7 +104,111 @@ void setup_gpio()
   pinMode(IN_OPENED, INPUT);
   pinMode(IN_BARRIER, INPUT);
   pinMode(IN_CURRENT, INPUT);
+
+  debouncer_in_closed.attach(IN_CLOSED);
+  debouncer_in_closed.interval(100); // interval in ms
+
+  debouncer_in_opened.attach(IN_OPENED);
+  debouncer_in_opened.interval(100); // interval in ms
+
+  debouncer_in_barrier.attach(IN_BARRIER);
+  debouncer_in_barrier.interval(100); // interval in ms
 }
+
+
+
+void loop()
+{
+    Knx.task();
+    debouncer_in_closed.update();
+    debouncer_in_opened.update();
+    debouncer_in_barrier.update();
+    unsigned long currentTime = millis();
+    
+    // only do measurements and other sketch related stuff if not in programming mode
+    if (Konnekting.isReadyForApplication())
+    {
+      if(currentTime > mytime + 25)
+      {
+        int in_current_raw = analogRead(IN_CURRENT);
+      
+        mytime = currentTime;
+
+
+       
+        // Handling of Inputs
+        in_closed = debouncer_in_closed.read();
+
+
+
+
+         // Handling of Outputs
+
+        if(out_openclose_cnt > 0)
+        {
+          digitalWrite(OUT_OPENCLOSE, HIGH);
+          out_openclose_cnt--;
+        }
+        else
+        {
+          digitalWrite(OUT_OPENCLOSE, LOW);
+        }
+      }
+    }
+      
+      if(currentTime > mytime2 + 5000)
+      {
+        
+        mytime2 = currentTime;
+      }
+    }
+}
+
+void callT()
+{
+  unsigned long currentMillis = millis();
+
+  if(calculateElapsedMillis(T1_last_run, currentMillis) >= T1_CYCLETIME)
+  {
+    T1();
+    //ToDo: Diag runtime of slice
+  }
+  else if(calculateElapsedMillis(T2_last_run, currentMillis) >= T2_CYCLETIME)
+  {
+    T2();
+  }
+}
+
+void T1()
+{
+  
+}
+void T2()
+{
+  
+}
+void T3()
+{
+  
+}
+void T4()
+{
+  
+}
+void T5()
+{
+  
+}
+
+unsigned long calculateElapsedMillis(unsigned long lastrunMillis, unsigned long currentMillis)
+{
+  if(currentMillis > lastrunMillis)
+    return currentMillis-lastrunMillis;
+  else // this means, there was an overflow
+    return (0xFFFFFFFF-lastrunMillis)+currentMillis;
+} 
+
+
 
 void reboot()
 {
@@ -103,28 +235,5 @@ void reboot()
     }
 #endif    
 
-}
-
-void loop()
-{
-    Knx.task();
-    unsigned long currentTime = millis();
-    
-    // only do measurements and other sketch related stuff if not in programming mode
-    if (Konnekting.isReadyForApplication())
-    {
-      if(currentTime > mytime + 25)
-      {
-        int in_current_raw = analogRead(IN_CURRENT);
-      
-        mytime = currentTime;
-      }
-      
-      if(currentTime > mytime2 + 5000)
-      {
-        
-        mytime2 = currentTime;
-      }
-    }
 }
 
